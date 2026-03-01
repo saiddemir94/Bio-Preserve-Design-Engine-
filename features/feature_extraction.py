f7301aa7945b106997e07faac5b2cc1fcc621c77
Said Taha Demir <saiddemir94@gmail.com>
2026-03-01 13:38:53 +0300

    Changes

--- features/feature_extraction.py
+++ features/feature_extraction.py
@@ -1,1 +1,28 @@
+# features/feature_extraction.py
 
+# Amino asit yuk bilgileri (basitlestirilmis)
+charge_map = {
+    'K': 1, 'R': 1, 'H': 1,   # pozitif
+    'D': -1, 'E': -1         # negatif
+}
+
+hydrophobic = set(['A','V','I','L','M','F','W','Y'])
+
+def extract_features(sequence):
+    length = len(sequence)
+
+    net_charge = sum(charge_map.get(aa, 0) for aa in sequence)
+
+    hydrophobic_count = sum(1 for aa in sequence if aa in hydrophobic)
+    hydrophobic_ratio = hydrophobic_count / length
+
+    return {
+        "length": length,
+        "net_charge": net_charge,
+        "hydrophobic_ratio": hydrophobic_ratio
+    }
+
+# test
+if __name__ == "__main__":
+    seq = "KKLRLLRIFKFLRLLKIFRK"
+    print(extract_features(seq))
