f0f95eb62c115448dd4c673c9a00c251dff51efc
Said Taha Demir <saiddemir94@gmail.com>
2026-03-01 13:48:19 +0300

    sumary changces

--- ml/train_model.py
+++ ml/train_model.py
@@ -1,1 +1,17 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
+# veri yükle
+df = pd.read_csv("data/amp_dataset.csv")
+
+X = df[["length", "net_charge", "hydrophobic_ratio"]]
+y = df["label"]
+
+model = RandomForestClassifier(n_estimators=50)
+model.fit(X, y)
+
+# test
+test_peptide = [[20, 6, 0.5]]
+prediction = model.predict(test_peptide)
+
+print("Antimikrobiyal olasılığı:", prediction[0])
