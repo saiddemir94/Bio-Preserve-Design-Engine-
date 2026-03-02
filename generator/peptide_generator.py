c6bc25bbd3674b5e2104d2171369dfd28533fdd2
Said Taha Demir <62424434+saiddemir94@users.noreply.github.com>
2026-03-02 16:54:19 +0300

    Delete generator directory

--- generator/peptide_generator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-amino_acids = list("ACDEFGHIKLMNPQRSTVWY")
-
-def generate_peptide(length=20):
-    return "".join(random.choice(amino_acids) for _ in range(length))
-
-for _ in range(3):
-    print(generate_peptide())
